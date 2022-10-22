50278d07344fbc311b2c926403bde1b925a5e4b0
Mellownyn <sunnyshettigar@gmail.com>
2022-10-22 10:34:09 -0500

    Build playerSelection function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-
-
 // Utility function that will return a random number between 0 and inputed max value
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -12,11 +10,32 @@ function getComputerChoice() {
     return computerSelection;
 }
 
+// Function to recieve player's input
+function getPlayerChoice() {
+    let playerSelection = prompt("Enter your choice").toLowerCase();
+    return playerSelection;
+}
+
+// Function to play a round of rock paper scissors
+function playRound(playerSelection, computerSelection) {
+    // accomodate ties
+    // rock beats scissor
+    // scissor beats paper
+    // paper beats rock
+    // naive approach, check every condition
+    if (playerSelection === computerSelection) {
+        console.log(`Tie: You and the comp both picked ${playerSelection}`);
+    }
+    
+}
+
 
 // Function tester
 for (let i = 0; i < 5; i++) {
     let testNum = i + 1;
     let computerSelection = getComputerChoice();
-    let result = `Test ${testNum} result: ${computerSelection}`;
+    let playerSelection = getPlayerChoice();
+    let result = `Test ${testNum} | Computer Result: ${computerSelection} | Player Result: ${playerSelection}`;
     console.log(result)
 }
+
